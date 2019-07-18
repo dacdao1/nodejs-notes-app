4e55469362daf4ad767c1b11a8c75e6d8c828674
DacD <dacdao01@gmail.com>
2019-07-18 19:36:05 -0400

    main js file

---
+++ app.js
@@ -0,0 +1,16 @@
+// const fs = require('fs')
+//
+// fs.writeFileSync('notes.txt', 'hello world')
+//
+// fs.appendFileSync('notes.txt', ' blah blah')
+
+// const add = require('./utils.js')
+// // const name = 'Andrew'
+//
+// const sum = add(5,4)
+// console.log(sum)
+
+
+const getNotes = require('./notes.js')
+
+console.log(getNotes())
