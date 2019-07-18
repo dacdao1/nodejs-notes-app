eb09e7c1ae3217d9d03098fc8594a6174e11c564
DacD <dacdao01@gmail.com>
2019-07-18 19:36:35 -0400

    example files, not important

---
+++ utils.js
@@ -0,0 +1,11 @@
+// This file is use to test out the module export and creating function within this file. DO NOT NEED FOR FUTURE USE.
+
+
+console.log('utils.js')
+
+const name = 'dac'
+
+const add = function (x, y) {
+  return x + y
+}
+module.exports = add
